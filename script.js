9d578e5c4b3bbe539f4ab5ce45625d1d3eb56463
Glitch (hello-webpage) <none>
2021-01-17 03:00:35 +0000

    🐦🍣 Checkpoint ./README.md:26970366/645 ./script.js:26970366/850 ./index.html:26970366/613

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+// Can I:
+// 1. locate the 'container' class'ed element from the html DOM
+// 2. add event listeners for mousedown / mouseup events
+// 3. respond to mousedown events / mouseup events by modifying
+//    some variable that I then can
+// 4. print out to console?
+let isMouseDown = false;
+
+init();
+
+function init () {
+  const container = document.getElementById("container");
+  container.addEventListener("mousedown", e => {
+    isMouseDown = true;
+    doClick(e);
+  })
+  
+  container.addEventListener("mouseup", e => {
+    isMouseDown = false;
+  })
+}
+
+function doClick(event) {
+  let tgt;
+  tgt = event.target;
+  console.log(tgt);
+  console.log(typeof(tgt));
+}
