12a662b844b86db60bbeb50c783bb4ff457a4282
Glitch (hello-webpage) <none>
2021-01-20 18:33:19 +0000

    🍢🛩 Checkpoint ./script.js:26970366/696 ./index.html:26970366/9

--- script.js
+++ script.js
@@ -13,8 +13,21 @@ function random(min, max) {
 const rand_x = random(3,6);
 const rand_y = random(3,6);
 
-/* double loop. Loosely speaking:
+/* double loop. Loosely speaking
+(using 'ul' for unordered list won't really do it because
+this will be bullet-pointed like a text list)
 1. for each i in range(rand_x), create a column
-    (represented generically as an unordered list 'ul'?)
 2. then, to that i'th col, add for each j in range(rand_y)
+   one button element
 */
+for (let i = 0; i < rand_x; i++) {
+  // 'span' is an HTML element type for generic containers
+  let button_col = document.createElement('span');
+  // give it a descriptive class
+  button_col.className = "button_col";
+  // now, fill it with buttons
+  for (let j = 0; j < rand_y; j++) {
+    var button = document.createElement('button');
+    button_col.appendChild(button);
+  }
+}
