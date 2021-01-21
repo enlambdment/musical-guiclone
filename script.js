56c4d90bb6bcf7b6a42fc3818d184d349269616d
Glitch (hello-webpage) <none>
2021-01-21 04:44:41 +0000

    🏙🏩 Checkpoint ./style.css:26970366/102 ./index.html:26970366/102 ./script.js:26970366/490

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // const canvas = document.querySelector('canvas');
 // const ctx = canvas.getContext('2d');
-const body = document.querySelector('body');
+const body = document.getElementById('container');
 
-// function to generate random integer within a range
-function random(min, max) {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min;
-  return num
-}
+// // function to generate random integer within a range
+// function random(min, max) {
+//   const num = Math.floor(Math.random() * (max - min + 1)) + min;
+//   return num
+// }
 
-// how to define a constructor for button elements
-// that I can then populate into a grid?
-const rand_x = random(3, 6);
-const rand_y = random(3, 6);
+// // how to define a constructor for button elements
+// // that I can then populate into a grid?
+// const rand_x = random(3, 6);
+// const rand_y = random(3, 6);
+
+const rand_x = 2;
+const rand_y = 3;
 
 /* double loop. Loosely speaking
 (using 'ul' for unordered list won't really do it because
@@ -20,18 +23,18 @@ this will be bullet-pointed like a text list)
 2. then, to that i'th col, add for each j in range(rand_y)
    one button element
 */
-for (let i = 0; i < rand_x; i++) {
-  // 'span' is an HTML element type for generic containers
-  let button_col = document.createElement('span');
-  // give it a descriptive class
-  button_col.className = "button_col";
-  // now, fill it with buttons
-  for (let j = 0; j < rand_y; j++) {
-    var button = document.createElement('button');
-    button_col.appendChild(button);
-  }
-  document.body.append(button_col);
-}
+// for (let i = 0; i < rand_x; i++) {
+//   // 'span' is an HTML element type for generic containers
+//   let button_col = document.createElement('span');
+//   // give it a descriptive class
+//   button_col.className = "button_col";
+//   // now, fill it with buttons
+//   for (let j = 0; j < rand_y; j++) {
+//     var button = document.createElement('button');
+//     button_col.appendChild(button);
+//   }
+//   document.body.append(button_col);
+// }
 
 /* 
  So far, each button_col is getting
