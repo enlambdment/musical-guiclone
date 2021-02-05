767a2258895115e55301c28c17bfd2ee3dc7dc96
Glitch (hello-webpage) <none>
2021-02-05 04:28:29 +0000

    😎🐿 Checkpoint ./script.js:26970366/848

---
+++ script.js
@@ -0,0 +1,25 @@
+/* To introduce the main interactivity with the musical
+   GUI grid, we will need to:
+  a) introduce an event listener on the container el, but
+  b) also make sure, in the callback fn provided to the 
+     event listener, that the type and / or class of the 
+     event (click) target is checked, and functionality 
+     only carried out if the target was indeed a button.
+     This is the safest / most principled thing to do, I think.
+*/
+
+const container = document.getElementById('container');
+container.addEventListener('mousedown', clickCell);
+
+function clickCell(event) {
+  // obtain event target
+  let tgt = event.target;
+  
+  // if target was not a button, exit
+  if (tgt.tagName !== "BUTTON") {
+    return;
+  } else {
+    // do some stuff
+    return;
+  }
+}
