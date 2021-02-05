26ad531cb3f8f82dd4be7381f1ff55ace7f0b07d
Glitch (hello-webpage) <none>
2021-02-05 05:13:50 +0000

    📈👋 Checkpoint ./script.js:26970366/787

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 /* We need a SoundFontPlayer */
-// let player = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
+let player = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
+
+/* You have to set up the player with a callback object
+   before you can use it.
+   The coconet GUI code in script.js does this inside an init()
+   function which also handles set up for event listeners.
+   But maybe I don't have to do it that way.
+*/
+let playerHardStop = false;
+
+player.callbackObject = {
+  run: (note) => buttonGrid.playStep(note),
+  stop: () => {
+    if (playerHardStop) {
+      stop();
+    } else {
+      play();
+    }
+  }
+} 
 
 /* Create a grid of buttons: this is the musical GUI */
 const buttonGrid = new ButtonGrid();
@@ -28,12 +47,18 @@ function clickCell(event) {
     // 1. get the position (row and col) of the selected button
     const row = parseInt(tgt.dataset.row);
     const col = parseInt(tgt.dataset.col);
+    const pitch = parseInt(tgt.dataset.pitch);
     
     // 2. toggle the cell at this position, so we can know the updated
     //    cell state.
     buttonGrid.toggleCell(row, col);
     
     // 3. if the cell has become activated (turned on), sound out its pitch.
-    
+    //    Use the row / col values to access the corresponding part of the
+    //    board's data.
+    const dot = buttonGrid.data[row][col];
+    if (dot.on === 1) {
+      player.playNoteDown({pitch: pitch, velocity: 80});
+    }
   }
 }
