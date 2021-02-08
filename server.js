ea3c0c2c8627384b87f4be748f6ca48be60c8f87
Glitch (hello-webpage) <none>
2021-02-08 22:49:06 +0000

    🍖🏅 Checkpoint ./server.js:26970366/1027 ./webpack.config.js:26970366/151

--- server.js
+++ server.js
@@ -8,6 +8,25 @@
 
 // init project
 const express = require('express')
+
+// Creates an Express app.
 const app = express() 
 
-// What exactly is the role of Express here?
+// When the app GETs the route specified in the 1st param,
+// here just '/', then it should carry out the callback
+// process specified in the 2nd param.
+// Here, our callback is a process that is totally indifferent
+// to the type / contents of the request, and just serves back
+// the generated & bundle-injected version of our original
+// index.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/dist/index.html')
+})
+
+// Listen for requests.
+// (Otherwise, there will never be an occasion for the 
+// callback to be run, because the above instruction just
+// sets up the callback as an asynchronous process)
+const listener = app.listen(
+  process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`)})
--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,14 @@ module.exports = {
   // where webpack should create the bundle(s) and what to call them.
   output: {
     filename: 'main.js',
+    // obtain an absolute path for where to place the created bundle 
+    // by prepending val. of __dirname to 'dist'
     path: path.resolve(__dirname, 'dist')
   },
   // tell webpack how to transform files other than JS files - e.g. here, CSS files
   module: {
     rules: [
-      { test: /.css$/, loader: "style-loader!css-loader",
+      { test: /.css$/, use: "css-loader",
         include: __dirname + '/src' }
     ]
   },
