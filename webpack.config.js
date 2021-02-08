c08278476cfa20fca81883c14b427a2e3681957e
Glitch (hello-webpage) <none>
2021-02-08 22:37:15 +0000

    💺✌️ Checkpoint ./webpack.config.js:26970366/999 ./server.js:26970366/1

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const webpack = require('webpack'); // to access built-in plugins
+const path = require('path'); // file path utilities
+
+module.exports = {
+  // which start file(s) to build the dep graph from
+  entry: {
+    main: './src/index.js'
+  },
+  // where webpack should create the bundle(s) and what to call them.
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  // tell webpack how to transform files other than JS files - e.g. here, CSS files
+  module: {
+    rules: [
+      { test: /.css$/, loader: "style-loader!css-loader",
+        include: __dirname + '/src' }
+    ]
+  },
+  // using html-webpack-plugin, let's generate a new html
+  // templating off of the ones from views, and inject the
+  // webpack-generated bundle into there
+  plugins: [
+    new HtmlWebpackPlugin({template: './views/index.html'})
+  ]
+};
